1a90ae3c86c7bdaecfe06cf5cf4173dd7404f84c
Mikolaj Szczepanski <mikolaj.szczepanski@outlook.com>
2018-04-22 11:25:27 +0200

    add rest api places test

--- server/uberTravel/api/tests.py
+++ server/uberTravel/api/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from rest_framework.test import APIRequestFactory,APIClient
 
-# Create your tests here.
+class PlacesViewTests(TestCase):
+    def test_get_status_code_is_200(self):
+        client = APIClient()
+        response = client.get('/api/places/')
+        print(response)
+        self.assertTrue(response.status_code == 200,'Incorrect response status code {0}'.format(response.status_code))
