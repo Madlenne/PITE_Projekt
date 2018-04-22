00417e9d3ff749795caf249cd2a58bf1133bd9ef
Wojciech Majkut <majkutw@gmail.com>
2018-04-22 13:22:49 +0200

    heroku deployment update

--- uberTravel/settings.py
+++ uberTravel/settings.py
@@ -118,9 +118,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+
 
 # Simplified static file serving.
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
