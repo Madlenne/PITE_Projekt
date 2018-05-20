e303a3bf4b3079f36fd480ff692f5a63b15d795f
Wojciech Majkut <majkutw@gmail.com>
2018-05-20 16:31:17 +0200

    improvements

--- api/views.py
+++ api/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import Group
+"""from django.contrib.auth.models import Group
 from api.models import Place, User
 from rest_framework import viewsets,status,generics
 from rest_framework.response import Response
@@ -33,4 +33,90 @@ class UserView(APIView):
             serializer.save()
             return Response(serializer.data)
 
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)"""
+
+from rest_framework import generics, mixins
+from api.models import Place, User
+from api.serializers import PlaceSerializer,UserSerializer
+from django.db.models import Q
+
+class PlaceView(mixins.CreateModelMixin, generics.ListAPIView):
+    pass
+    lookup_field        = 'placeId'
+    serializer_class    = PlaceSerializer
+
+    def get_queryset(self):
+        qs = Place.objects.all()
+        query = self.request.GET.get("q")
+        if query is not None:
+            qs = qs.filter(Q(name__icontains=query)|Q(vicinty__icontains=query)).distinct()
+        return qs
+
+    def post(self,request,*args,**kwargs):
+        return self.create(request, *args, **kwargs)
+
+    def get_serializer_context(self, *args, **kwargs):
+        return {"request": self.request}
+
+
+class UserView(mixins.CreateModelMixin, generics.ListAPIView):
+    pass
+    lookup_field        = 'user_id'
+    serializer_class    = UserSerializer
+    
+    def get_queryset(self):
+        qs = User.objects.all()
+        query = self.request.GET.get("q")
+        if query is not None:
+            qs = qs.filter(Q(user_id__icontains=query))
+        return qs
+
+    def post(self,request,*args,**kwargs):
+        return self.create(request, *args, **kwargs)
+
+    def get_serializer_context(self, *args, **kwargs):
+        return {"request": self.request}
+    """def get(self, request, user_id):
+        try:
+            user = User.objects.get(user_id=user_id)
+        except User.DoesNotExist:
+            user = User(user_id=user_id,is_guide=False)
+            user.save()
+            
+        return Response(UserSerializer(user).data)
+
+    def put(self, request, user_id):
+        try:
+            user = User.objects.get(user_id=user_id)
+        except User.DoesNotExist:
+            user = User(user_id=user_id,is_guide=False)
+            user.save()
+
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)"""
+
+class PlaceRudView(generics.RetrieveUpdateDestroyAPIView):
+    pass
+    lookup_field        = 'placeId'
+    serializer_class    = PlaceSerializer
+
+    def get_queryset(self):
+        return Place.objects.all()
+
+    def get_serializer_context(self, *args, **kwargs):
+        return {"request": self.request}
+
+class UserRudView(generics.RetrieveUpdateDestroyAPIView):
+    pass
+    lookup_field        = 'user_id'
+    serializer_class = UserSerializer
+    
+    def get_queryset(self):
+        return User.objects.all()
+
+    def get_serializer_context(self, *args, **kwargs):
+        return {"request": self.request}
