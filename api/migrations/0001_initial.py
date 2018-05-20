a5e4efb5a2283a899e7aab9820025a45ec3887f3
Wojciech Majkut <majkutw@gmail.com>
2018-05-20 20:40:06 +0200

    update

--- api/migrations/0001_initial.py
+++ api/migrations/0001_initial.py
@@ -16,8 +16,19 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Place',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(max_length=500)),
+                ('name', models.CharField(max_length=200,default="none",null=True)),
+                ('photoRef', models.CharField(max_length=500,default="none")),
+                ('placeId',models.CharField(max_length=200,primary_key=True,default="none")),
+                ('vicinty',models.CharField(max_length=500,default="none")),
+                ('latitude',models.FloatField(default=0)),
+                ('longitude',models.FloatField(default=0)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='User',
+            fields=[
+                ('user_id', models.CharField(max_length=200, primary_key=True, serialize=False)),
+                ('is_guide', models.BooleanField(default=False)),
             ],
         ),
     ]
